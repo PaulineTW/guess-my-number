4f6a55ee9270f3a01c527f66afbf8841509b40c2
pauline rouger <paulinerouge@hotmail.com>
2022-04-28 12:43:04 +0800

    Add addEventListener to check class button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+//experiment
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if(!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!'
+  }
+});
