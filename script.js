7dd7b3543e3c7b5f82a2a52446a13a3530a1b112
pauline rouger <paulinerouge@hotmail.com>
2022-04-28 12:59:07 +0800

    Generate random number + Compare Number and user input

--- script.js
+++ script.js
@@ -13,10 +13,21 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+// Generate random number(secretNumber)
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+//Compare secretNumber and user input(guess)
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   if(!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!'
+  } else if (guess === secretNumber) {
+  document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too Hight!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low!';
   }
 });
